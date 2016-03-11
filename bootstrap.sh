bba57b60f620fea7af0f1a28afb67b7a8d153174
lukhar <lukasz.har@gmail.com>
2016-03-11 15:13:43 +0000

    Added bootstrap script for dot files.

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf ~/.config/.bashrc ~/.bashrc
+ln -sf ~/.config/.pentadactylrc ~/.pentadactylrc
+ln -sf ~/.config/ptpython ~/.ptpython
+ln -sf ~/.config/.pypirc ~/.pypirc
+ln -sf ~/.config/.tmux.conf ~/.tmux.conf
+ln -sf ~/.config/.vimperatorrc ~/.vimperatorrc
+ln -sf ~/.vim/vimrc ~/.vimrc
