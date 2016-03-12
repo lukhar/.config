12d2924d8dc88047cb78b65321055f9a58a3a485
lukhar <lukasz.har@gmail.com>
2016-03-12 00:20:53 +0000

    Unified config file naming

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 ln -sf ~/.config/.bashrc ~/.bashrc
 ln -sf ~/.config/.pentadactylrc ~/.pentadactylrc
-ln -sf ~/.config/ptpython ~/.ptpython
+ln -sf ~/.config/.ptpython ~/.ptpython
 ln -sf ~/.config/.pypirc ~/.pypirc
 ln -sf ~/.config/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/.config/.vimperatorrc ~/.vimperatorrc
