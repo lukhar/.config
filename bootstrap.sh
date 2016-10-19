e32caafcd5f3738cf79f25a61bf4d4556cf3284a
lukhar <lukasz.har@gmail.com>
2016-10-19 23:10:07 +0100

    Updated bootstraping file and add yaurt config to disable AUR questions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+ln -sf ~/.config/.agignore ~/.agignore
 ln -sf ~/.config/.bashrc ~/.bashrc
-ln -sf ~/.config/.profile ~/.profile
+ln -sf ~/.config/.gitignore ~/.gitignore
+ln -sf ~/.config/.inputrc ~/.inputrc
 ln -sf ~/.config/pentadactyl/pentadactylrc ~/.pentadactylrc
+ln -sf ~/.config/.profile ~/.profile
 ln -sf ~/.config/.ptpython ~/.ptpython
 ln -sf ~/.config/.pypirc ~/.pypirc
 ln -sf ~/.config/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/.config/.vimperatorrc ~/.vimperatorrc
-ln -sf ~/.vim/vimrc ~/.vimrc
-ln -sf ~/.config/.Xresources ~/.Xresources
 ln -sf ~/.config/.Xresources ~/.Xdefaults
+ln -sf ~/.config/.Xresources ~/.Xresources
+ln -sf ~/.config/.yaourtrc ~/.yaourtrc
+ln -sf ~/.vim/gvimrc ~/.gvimrc
+ln -sf ~/.vim/vimrc ~/.vimrc
