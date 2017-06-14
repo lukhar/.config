1d93e67fde8b0529b2bea9057defe3ce921cecc6
lukhar <lukasz.har@gmail.com>
2017-06-14 01:27:17 +0100

    Leverage yank plugin instead of per system customizations.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,6 @@ ln -sf ~/.config/.profile ~/.profile
 ln -sf ~/.config/.ptpython ~/.ptpython
 ln -sf ~/.config/.pypirc ~/.pypirc
 ln -sf ~/.config/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/.config/tmux/tools/safe-reattach-to-user-namespace ~/bin/safe-reattach-to-user-namespace
 ln -sf ~/.config/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/.config/.Xresources ~/.Xdefaults
 ln -sf ~/.config/.Xresources ~/.Xresources
