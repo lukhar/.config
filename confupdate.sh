e5d2581e76e3837e1bd07ba1c0a2f875d754a481
lukhar <lukasz.har@gmail.com>
2017-10-19 21:47:53 +0100

    Disabled unstashing changes.

--- confupdate.sh
+++ confupdate.sh
@@ -2,8 +2,9 @@
 
 for repository in "$HOME/.config" "$HOME/.vim"; do
 	cd "${repository}" &&
+
+	echo "Stashing changes in: $repository"
 	git stash &&
 	git submodule foreach git pull origin master &&
-	git pull origin master &&
-	git stash pop
+	git pull origin master
 done
