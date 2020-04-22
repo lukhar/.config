2620521c9c72309aafe04e819245cf24bb224799
lukhar <lukasz.har@gmail.com>
2020-04-22 23:43:13 +0100

    enforcing noto fonts was actually a good idea after all

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -28,6 +28,8 @@ else:
     c.fonts.prompts = "9pt sans-serif"
     c.fonts.statusbar = "9pt monospace"
     c.fonts.tabs = "9pt monospace"
+    c.fonts.web.family.standard = "NotoSans Nerd Font"
+    c.fonts.web.family.fixed = "DroidSansMono Nerd Font"
 
 c.url.searchengines = {
     "g": "https://encrypted.google.com/search?q={}",
