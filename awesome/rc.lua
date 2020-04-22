5fb6922b67e60af34c34caeedc07e75a0da3f049
lukhar <lukasz.har@gmail.com>
2020-04-22 15:53:09 +0100

    wm and qt5ct adjustments

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -51,7 +51,7 @@ beautiful.bg_normal         = "#222D32"
 beautiful.bg_focus          = "#2C3940"
 beautiful.titlebar_close_button_normal = awful.util.get_themes_dir() .. "cesious/titlebar/close_normal_adapta.png"
 beautiful.titlebar_close_button_focus = awful.util.get_themes_dir() .. "cesious/titlebar/close_focus_adapta.png"
-beautiful.font              = "Noto Sans Regular 10"
+beautiful.font              = "Noto Sans Regular 9"
 beautiful.notification_font = "Noto Sans Bold 12"
 
 -- This is used later as the default terminal and editor to run.
