b004ae3d1270c5d9bf860e96a2bda064d35591bd
lukhar <lukasz.har@gmail.com>
2020-04-22 16:09:24 +0100

    added dm configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ mkdir -p ~/sdk
 
 ln -sf ~/.config/.agignore ~/.agignore
 ln -sf ~/.config/.bashrc ~/.bashrc
+ln -sf ~/.config/.dmrc ~/.dmrc
 ln -sf ~/.config/.zshrc ~/.zshrc
 ln -sf ~/.config/.bash_profile ~/.bash_profile
 ln -sf ~/.config/.bash_aliases ~/.bash_aliases
