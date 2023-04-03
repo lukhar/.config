6b0efffd0d65b983180291633d3f4c7f878ce228
lukhar <lukasz.har@gmail.com>
2023-04-03 02:32:45 +0100

    chore: ensure pyenv and ziti configurations are updated as well

--- confupdate.sh
+++ confupdate.sh
@@ -8,3 +8,6 @@ for repository in "$HOME/.config" "$HOME/.vim"; do
 	git submodule foreach git pull origin master &&
 	git pull origin master
 done
+
+[ -x "$(command -v pyenv)" ] && pyenv update
+[ -x "$(command -v zinit)" ] && zinit update
