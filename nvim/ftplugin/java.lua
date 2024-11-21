2e42ea6f301ffd217f5640088c98115854938253
lukhar <lukasz.har@gmail.com>
2024-11-20 20:27:07 -0800

    chore(neovim): tuned the `jdtls` configuration
    * large initial heap size (20 GB)
    * custom workspace per project

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,5 +1,8 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace = '~/.cache/jdtls/workspace/' .. project_name
+
 local config = {
-    cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls') },
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls'), '-data', workspace ,  '-Xmx20g'},
+  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
