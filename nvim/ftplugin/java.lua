e919ea485c53f679ed722d25b25f4b6e3a4dd821
lukhar <lukasz.har@gmail.com>
2024-12-01 22:25:05 -0500

    fix(neovim): ensured `jdtls` resolves home directory proprely
    Additionally:
    * more detailed cofniguread for the LSP server

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,8 +1,21 @@
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace = '~/.cache/jdtls/workspace/' .. project_name
+local workspace = vim.env.HOME .. '/.cache/jdtls/workspace/' .. project_name
 
 local config = {
-  cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls'), '-data', workspace ,  '-Xmx20g'},
+  cmd = {
+    vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls'),
+    '-data',
+    workspace,
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx20g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+  },
   root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
