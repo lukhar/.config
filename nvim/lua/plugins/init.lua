1eb826e095d1fe3d20e950a23df4746c4ca0ad28
lukhar <lukasz.har@gmail.com>
2025-07-06 15:22:03 +0100

    chore(neovim): updated plugins and disabled markdown renderer for vscode

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -29,6 +29,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    enabled = not vim.g.vscode,
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
