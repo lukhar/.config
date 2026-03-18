81fb2c35b8b64453a05826935a4bd4d9fb4db65f
lukhar <lukasz.har@gmail.com>
2026-03-18 15:50:19 +0000

    chore(nvim): upgraded plugins removed custom colors from solarized.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,25 +13,25 @@ return {
           \ . ">"<CR>
     ]])
 
-    require('solarized').setup({
-      highlights = function(colors)
-        return {
-          LineNr = { fg = colors.base1, bg = colors.base02 },
-          CursorLineNr = { bg = colors.base02 },
-          CursorLine = { bg = colors.base02 },
-          Visual = { fg = colors.base03, bg = colors.base01 },
-          CurSearch = { fg = colors.base03, bg = colors.orange },
-          Search = { fg = colors.base03, bg = colors.change },
-          diffAdded = { fg = colors.hint },
-          diffChanged = { fg = colors.warning },
-          diffRemoved = { fg = colors.delete },
-          diffLine = { fg = colors.info },
-          diffSubname = { fg = colors.orange },
-          fugitiveUnstagedHeading = { fg = colors.orange },
-          fugitiveStagedHeading = { fg = colors.orange },
-          fugitiveHeading = { fg = colors.orange },
-        }
-      end,
-    })
+    -- require('solarized').setup({
+    --   highlights = function(colors)
+    --     return {
+    --       LineNr = { fg = colors.base1, bg = colors.base02 },
+    --       CursorLineNr = { bg = colors.base02 },
+    --       CursorLine = { bg = colors.base02 },
+    --       Visual = { fg = colors.base03, bg = colors.base01 },
+    --       CurSearch = { fg = colors.base03, bg = colors.orange },
+    --       Search = { fg = colors.base03, bg = colors.change },
+    --       diffAdded = { fg = colors.hint },
+    --       diffChanged = { fg = colors.warning },
+    --       diffRemoved = { fg = colors.delete },
+    --       diffLine = { fg = colors.info },
+    --       diffSubname = { fg = colors.orange },
+    --       fugitiveUnstagedHeading = { fg = colors.orange },
+    --       fugitiveStagedHeading = { fg = colors.orange },
+    --       fugitiveHeading = { fg = colors.orange },
+    --     }
+    --   end,
+    -- })
   end,
 }
