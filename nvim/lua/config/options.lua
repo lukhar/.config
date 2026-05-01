bd680a466f391597fd1a1c236d30282b17630fa5
lukhar <lukasz.har@gmail.com>
2026-04-30 18:38:15 -0700

    fix(neovim): replaced deprecated highlight call.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -44,7 +44,7 @@ vim.opt.clipboard = 'unnamed'
 vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
   callback = function()
-    vim.highlight.on_yank({ timeout = 150 })
+    vim.hl.on_yank({ timeout = 150 })
   end,
   group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
 })
